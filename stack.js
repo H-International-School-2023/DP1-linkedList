87feb49af243c9a3b3995239e2422503c894a23a
Andrea Beggiato <andrea.beggiato@h-is.com>
2021-11-25 12:51:50 +0100

    stack finished

--- stack.js
+++ stack.js
@@ -19,9 +19,11 @@ class Stack {
   push(item) {
     const newNode = new SingleNode(item, null);
     if (this.head === null) {
+      // If it's empty, add the new node to the head
       this.head = newNode;
     }
     else {
+      // else the newNode becomes the head that points to the old head.
       newNode.next = this.head;
       this.head = newNode;
     }
@@ -29,12 +31,18 @@ class Stack {
 
   pop() {
     // TODO: return the "first" item in the stack and remove it
+    const nodeToReturn = this.head;
+    this.head = nodeToReturn.next;
+    return nodeToReturn.data;
   }
 }
 
 const stack = new Stack();
 
 stack.push('Pringles 1');
+stack.push('Pringles 2');
+stack.push('Pringles 3');
+stack.push('Pringles 4');
 
 if (stack.isEmpty()) {
   console.log('is empty');
@@ -42,3 +50,6 @@ if (stack.isEmpty()) {
 else {
   console.log('is not empty');
 }
+
+console.log(stack.pop());
+console.log(stack.pop());
