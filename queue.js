6cac64ef5a4cc638418fddf98c567ab3562554f8
Andrea Beggiato <andrea.beggiato@h-is.com>
2021-12-02 12:10:10 +0100

    queue

---
+++ queue.js
@@ -0,0 +1,38 @@
+const Nodes = require('./nodes');
+
+const SingleNode = Nodes.SingleNode;
+
+class Queue {
+  constructor() {
+    this.head = null;
+  }
+
+  isEmpty() {
+    if (this.head === null) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  enqueue(item) {
+    const newNode = new SingleNode(item, null);
+    if (this.head === null) {
+      this.head = newNode;
+    }
+    else {
+      let currentNode = this.head;
+      while (currentNode.next != null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+  }
+
+  dequeue() {
+
+  }
+}
+
+const queue = new Queue();
